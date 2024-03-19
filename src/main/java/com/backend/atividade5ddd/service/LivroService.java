11ac24f9692eb7528d7e5dadbef0a7e20966328f
Thiago Martins <thiagomartins@audax.mobi>
2024-03-19 19:42:14 -0300

    adicionando explicação da camada service

--- src/main/java/com/backend/atividade5ddd/service/LivroService.java
+++ src/main/java/com/backend/atividade5ddd/service/LivroService.java
@@ -2,4 +2,5 @@ package com.backend.atividade5ddd.service;
 
 public class LivroService {
 
+	// aqui ficaria as funções de criação e manuseio da logica de livro, utilizando o livro como peça central
 }
